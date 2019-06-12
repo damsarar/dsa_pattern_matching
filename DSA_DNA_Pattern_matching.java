0cefd7bb0900cdc3dd0beb6cb6e541c3b16cb5ad
Damsara Ranasinghe <42701569+damsarar@users.noreply.github.com>
2019-06-12 21:59:45 +0530

    Third Commit

--- DSA_DNA_Pattern_matching.java
+++ DSA_DNA_Pattern_matching.java
@@ -1,15 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DSA_DNA_Pattern_matching {
     static List<String> datalines = new ArrayList<>();             // list to store read lines of DNA database
     static List<String> querylines = new ArrayList<>();            // list to store read lines of query base
     static Map<String, String> data = new LinkedHashMap<>();       // map to store DNA database (description, string) pairs
     static Map<String, String> queries = new LinkedHashMap<>();    // map to store query base (description, string) pairs
-    public final static int nalph = 4;                             // no of characters in the alphabet
+    public final static int nalph = 256;                             // no of characters in the alphabet
     static String tempkey;                                         // variable to store the description temporary
     static int foundAt;                                            // variable to store the value returned by the matching algorithm
     static int status;                                             // variable to store the matched status temporary
     static int primeno = 101;                                      // variable to store the prime number
+    static BufferedWriter fw;
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {            
             // reading the DNA database and the query base
             datalines = Files.readAllLines(Paths.get("DNA Database.txt"));
@@ -46,10 +60,28 @@ public class DSA_DNA_Pattern_matching {
         queries.remove("EOF");
         data.remove("EOF");
         
+        File file = new File("output.output");   // deleting the existing file
+        file.delete();
+        
+        try{    
+            fw=new BufferedWriter(new FileWriter("output.output", true));   //creating the file
+            fw.write("");
+            fw.flush();
+        }catch(Exception e){
+            System.out.println(e);
+        }    
+          
         // looping through the queries map
         queries.forEach((pdescription, pattern) -> {
             status = 0;
             System.out.println(pdescription);
+            try {
+                fw.flush();
+                fw.write(pdescription);
+                fw.newLine();
+            } catch (IOException ex) {
+                Logger.getLogger(DSA_DNA_Pattern_matching.class.getName()).log(Level.SEVERE, null, ex);
+            }
             
             // looping through the DNA database map
             data.forEach((tdescription, text) ->{
@@ -57,16 +89,34 @@ public class DSA_DNA_Pattern_matching {
                   if(foundAt != -1){
                       status = 1;
                       System.out.println("[" +tdescription + "]" + " at offset " + foundAt);
+                      try {
+                          fw.flush();
+                          fw.append("[" +tdescription + "]" + " at offset " + foundAt);     // writing to file
+                          fw.newLine();
+                          
+                      } catch (IOException ex) {
+                          Logger.getLogger(DSA_DNA_Pattern_matching.class.getName()).log(Level.SEVERE, null, ex);
+                      }
                   }              
             });
             
             if(status == 0){
                     System.out.println("NOT FOUND");
+                    try {
+                            fw.flush();
+                            fw.write("NOT FOUND");     // writing to file
+                            fw.newLine();
+                          
+                      } catch (IOException ex) {
+                          Logger.getLogger(DSA_DNA_Pattern_matching.class.getName()).log(Level.SEVERE, null, ex);
+                      }
             }  
-        });        
+        });    
+        
+        fw.close();     // closing the file
     }
     
-    // matching function - Rabin Karp algorithm 
+    
     static int rabinKarp(String p, String t, int prime){ 
         int m = p.length();         // length of the pattern
         int n = t.length();         // length of the text
--- DSA_Pattern_matching_with_wildcards.java
+++ DSA_Pattern_matching_with_wildcards.java
@@ -1,61 +1,171 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class DSA_Pattern_matching_with_wildcards {
-    static String tcontent, pcontent;
+    static String tcontent, pcontent, outputString;
 
     public static void main(String[] args) {        
+        System.out.println("1 - Matching pattern\n2 - Not matching pattern\n3 - Empty pattern and a text");
+        System.out.println("4 - Empty pattern and non-empty text\n5 - Empty text and non-empty pattern");
+        System.out.println("6 - Pattern with spaces\n7 - Pattern havin an underscore only");
+        System.out.println("Enter an option :");
         
+        Scanner s1 = new Scanner(System.in);
+        String opt = s1.next();
+        
+        switch(opt){
+            case "1":
+                // Matching pattern
+                readFile("q1/pattern1.txt", "q1/text1.txt");
+                patternSearch();
+                writeFile("q1/output1.output", outputString);                
+                
+                break;
+                
+            case "2":
+                // Not matching pattern
+                readFile("q1/pattern2.txt", "q1/text2.txt");
+                patternSearch();
+                writeFile("q1/output2.output", outputString);                
+                
+                break;
+                
+            case "3":
+                // Empty pattern and a text
+                readFile("q1/pattern3.txt", "q1/text3.txt");
+                patternSearch();
+                writeFile("q1/output3.output", outputString);               
+                
+                break;
+                
+            case "4":
+                // Empty pattern and non-empty text
+                readFile("q1/pattern4.txt", "q1/text4.txt");
+                patternSearch();
+                writeFile("q1/output4.output", outputString);                
+                
+                break;
+                
+            case "5":
+                // Empty text and non-empty pattern"
+                readFile("q1/pattern5.txt", "q1/text5.txt");
+                patternSearch();
+                writeFile("q1/output5.output", outputString);                
+                
+                break;
+                
+            case "6":
+                // Pattern with spaces
+                readFile("q1/pattern6.txt", "q1/text6.txt");
+                patternSearch();
+                writeFile("q1/output6.output", outputString);        
+                
+                break;
+                
+            case "7":
+                // Pattern havin an underscore only
+                readFile("q1/pattern7.txt", "q1/text7.txt");
+                patternSearch();
+                writeFile("q1/output7.output", outputString);               
+                
+                break;
+                
+            default:
+                System.out.println("Enter a correct option");
+                break;
+                    
+        } 
+    }
+   
+    // matching function - naive string matching
+    public static void patternSearch(){
+	outputString = "";                
+        // checking if text or pattern is empty
+        if(tcontent.isEmpty() && pcontent.isEmpty()){
+            System.out.println("Empty text and empty pattern inserted");
+            outputString+= "Empty text and empty pattern inserted";
+        }else if(tcontent.isEmpty()){
+            System.out.println("Empty text inserted");
+            outputString+= "Empty text inserted";
+        }else if(pcontent.isEmpty()){
+            System.out.println("Empty pattern inserted");
+            outputString+= "Empty pattern inserted";
+        }else{
+            String t[] = tcontent.split("");         // converting inputted string to a string array
+            String p[] = pcontent.split("");      // converting inputted string to a string array
+            
+            int n = t.length;
+            int m = p.length;
+            int matched = 0;
+            int i;
+       
+            // looping through the text
+            for(i=0; i<=n-m; i++){
+                int j;
+
+                //looping through the pattern
+                for(j=0; j<m; j++){                
+                    if(!(t[i+j].equals(p[j]))){       
+                        if(!(p[j].equals("_"))){
+                            break;                  // in here loop will break if there is a mismatch or the current character is not the '_'
+                        }
+                    }
+                }
+                if(j == m){     // if the j goes to the last index of the pattern, then the pattern is matched
+                    System.out.println("Pattern found at index :" + i);
+                    matched = 1;
+                    
+                    outputString += ("Pattern found at index :" + i + "\r\n");
+                   
+                }
+            }
+
+            if(matched == 0){
+                System.out.println("Pattern not found");    // when pattern not found
+                outputString = "Pattern not found";
+            }
+        }       
+    }
+    
+    
+    // Function to read a file
+    public static void readFile(String pname, String tname){
         try
         {
-            tcontent = new String ( Files.readAllBytes( Paths.get("text1.txt") ) );        // getting the text file
-            pcontent = new String ( Files.readAllBytes( Paths.get("pattern1.txt") ) );     // getting the pattern file
+            tcontent = new String ( Files.readAllBytes( Paths.get(tname) ) );        // getting the text file
+            pcontent = new String ( Files.readAllBytes( Paths.get(pname) ) );     // getting the pattern file
         }
         catch (IOException e)
         {
             e.printStackTrace();
-        }
-        
-        // checking if text or pattern is empty
-        if(tcontent.isEmpty() && pcontent.isEmpty()){
-            System.out.println("Empty text and empty pattern inserted");
-        }else if(tcontent.isEmpty()){
-            System.out.println("Empty text inserted");
-        }else if(pcontent.isEmpty()){
-            System.out.println("Empty pattern inserted");
-        }else{
-            String text[] = tcontent.split("");         // converting inputted string to a string array
-            String pattern[] = pcontent.split("");      // converting inputted string to a string array
-       
-            patternSearch(text, pattern);     // calling the matching function
-        }            
-    }
-   
-    // matching function - naive string matching
-    public static void patternSearch(String[] t, String[] p){
-	int n = t.length;
-	int m = p.length;
-	int matched = 0;
-	int i;
-        
-        // looping through the text
-	for(i=0; i<=n-m; i++){
-            int j;
-            
-            //looping through the pattern
-            for(j=0; j<m; j++){                
-                if(!(t[i+j].equals(p[j]))){       
-                    if(!(p[j].equals("_"))){
-                        break;                  // in here loop will break if there is a mismatch or the current character is not the '_'
-                    }
-                }
-            }
-            if(j == m){     // if the j goes to the last index of the pattern, then the pattern is matched
-                System.out.println("Pattern found at index :" + i);
-                matched = 1;                
-            }
-	}
-	
-	if(matched == 0){
-            System.out.println("Pattern not found");
-	}
+        }               
     }
     
+    
+    //Function to write to a file
+    public static void writeFile(String fname, String content){        
+        OutputStream os1 = null;
+        
+        File file = new File(fname);   // deleting the existing file
+        file.delete();
+        
+        try {
+            os1 = new FileOutputStream(new File(fname));
+            os1.write(content.getBytes(), 0, content.length());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                os1.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } 
+    }
+   
 }
